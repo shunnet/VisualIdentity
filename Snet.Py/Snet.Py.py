21f37eb080fe284c7a04af582c160d0d0b26f830
李顺 <vipls021@163.com>
2026-03-09 15:20:50 +0800

    更新依赖

--- Snet.Py/Snet.Py.py
+++ Snet.Py/Snet.Py.py
@@ -6,4 +6,8 @@ from ultralytics import YOLO
 model = YOLO("best.pt")  # load a custom trained model
 
 # Export the model
-model.export(format="onnx")
+# YOLOv5u–YOLOv12
+model.export(format="onnx",opset=17)
+
+# YOLOv26
+# model.export(format="onnx",opset=18)
